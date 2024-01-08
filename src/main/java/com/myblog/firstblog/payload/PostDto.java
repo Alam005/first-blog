df3ef397adb668f1e322def504ffca04a2345a5e
Alam <skhalam491@gmail.com>
2024-01-09 02:56:52 +0530

    modified pom.xml file added features in PostController add PostDto add PostRepository add PostService add PostServiceImpl

---
+++ src/main/java/com/myblog/firstblog/payload/PostDto.java
@@ -0,0 +1,15 @@
+package com.myblog.firstblog.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostDto {
+    private long id;
+    private String description;
+    private String content;
+    private String title;
+}
---
+++ src/main/java/com/myblog/firstblog/service/PostService.java
@@ -0,0 +1,7 @@
+package com.myblog.firstblog.service;
+
+import com.myblog.firstblog.payload.PostDto;
+
+public interface PostService {
+    public PostDto createPost(PostDto postDto);
+}
